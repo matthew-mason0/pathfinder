34328854bcddec58f1e49f559a178256fb6ff066
Matthew Mason <mxm1599@student.bham.ac.uk>
2026-01-07 08:41:12 +0000

    Created initial file structure

---
+++ backend/src/main/java/agent-navigation/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package uk.ac.bham.cs.git.mxm1599;
+
+/**
+ *
+ * @author matthew
+ */
+public class Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
