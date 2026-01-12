91d176b8f1eb91b5ce7f0271364081a5701b023e
matthew-mason0 <matthewmason2004@yahoo.co.uk>
2026-01-12 11:30:48 +0000

    Implemented gridRenderer.js in main.js, index.html

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,7 +1,20 @@
+let grid;
+let renderer;
+
 function setup() {
-  createCanvas(400, 400);
+	createCanvas(windowWidth, windowHeight);
+	grid = new Grid(10, 10);
+	renderer = new GridRenderer(40);
+	testVisuals();
 }
 
 function draw() {
-  background(220);
+  	background(50);
+	renderer.draw(grid);
+}
+
+function testVisuals() {
+	grid.getCell(2, 3).type = "WALL";
+	grid.getCell(0, 0).type = "START";
+	grid.getCell(9, 9).type = "GOAL";
 }
