c302251f9112f07631f8e2dbf12af3cbf3ba2776
matthew-mason0 <matthewmason2004@yahoo.co.uk>
2026-01-12 11:06:01 +0000

    Added GridRenderer class to gridRenderer.js

--- frontend/js/rendering/gridRenderer.js
+++ frontend/js/rendering/gridRenderer.js
@@ -23,3 +23,37 @@ class Grid {
         return this.cells[y][x];
     }
 }
+
+class GridRenderer {
+    constructor(cellSize) {
+        this.cellSize = cellSize;
+
+        this.colours = {
+            EMPTY: [255],
+            WALL: [40],
+            START: [0, 200, 0],
+            GOAL: [200, 0, 0],
+            VISITED: [150, 150, 255],
+            FRONTIER: [0, 150, 255],
+            PATH: [255, 255, 0],
+            AGENT: [255, 165, 0]
+        };
+    }
+
+    draw(grid) {
+        for (let i = 0; i < grid.rows; i++) {
+            for (let j = 0; j < grid.columns; j++) {
+                this.drawCell(grid.getCell(i, j));
+            }
+        }
+    }
+
+    drawCell(cell) {
+        let size = this.cellSize;
+        let colour = this.colours[cell.type] || [255]; // white if undefined
+        stroke(200);
+        strokeWeight(1);
+        fill(...colour);
+        rect(cell.x * size, cell.y * size, size, size);
+    }
+}
