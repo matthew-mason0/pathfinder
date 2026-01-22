faa6d2b1af661eb4e8c65e47ea0fe9b5192ab36c
matthew-mason0 <matthewmason2004@yahoo.co.uk>
2026-01-22 19:35:41 +0000

    Added BFS class to BFS.java

--- backend/src/agentnavigation/algorithms/BFS.java
+++ backend/src/agentnavigation/algorithms/BFS.java
@@ -1,10 +1,36 @@
 package agentnavigation.algorithms;
 
+import java.util.Queue;
+import java.util.ArrayDeque;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
+
 import agentnavigation.environment.Graph;
 import agentnavigation.environment.Node;
 
 public class BFS implements SearchAlgorithm {
-    public void search(Graph graph, Node node) {
-        
+
+    @Override
+    public void search(Graph graph, Node start) {
+        Queue<Node> queue = new ArrayDeque<>();
+        Set<Node> visited = new HashSet<>();
+
+        visited.add(start);
+        queue.add(start);
+        List<Node> traversalList = new ArrayList<>();
+
+        while (!queue.isEmpty()) {
+            Node current = queue.remove();
+            traversalList.add(current);
+
+            for (Node neighbour : graph.getNeighbours(current)) {
+                if (!visited.contains(neighbour)) {
+                    visited.add(neighbour);
+                    queue.add(neighbour);
+                }
+            }
+        }
     }
 }
