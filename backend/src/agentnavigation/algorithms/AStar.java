2de6f6fb255885deeb4aad449c8cd8d03a812d70
matthew-mason0 <matthewmason2004@yahoo.co.uk>
2026-01-22 19:41:51 +0000

    Redefined method signature for SearchAlgorithm interface dependants

--- backend/src/agentnavigation/algorithms/AStar.java
+++ backend/src/agentnavigation/algorithms/AStar.java
@@ -1,10 +1,13 @@
 package agentnavigation.algorithms;
 
+import java.util.List;
+
 import agentnavigation.environment.Graph;
 import agentnavigation.environment.Node;
 
 public class AStar implements SearchAlgorithm {
-    public void search(Graph graph, Node node) {
-        
+    public List<Node> search(Graph graph, Node node) {
+        // TODO implement AStar
+        return null;
     }
 }
--- backend/src/agentnavigation/algorithms/Dijkstra.java
+++ backend/src/agentnavigation/algorithms/Dijkstra.java
@@ -1,10 +1,13 @@
 package agentnavigation.algorithms;
 
+import java.util.List;
+
 import agentnavigation.environment.Graph;
 import agentnavigation.environment.Node;
 
 public class Dijkstra implements SearchAlgorithm {
-    public void search(Graph graph, Node node) {
-        
+    public List<Node> search(Graph graph, Node node) {
+        // TODO implement Dijkstra
+        return null;
     }
 }
