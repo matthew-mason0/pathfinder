19d5ccd52ef0552e1c7b29802cc8078a3b169cbc
matthew-mason0 <matthewmason2004@yahoo.co.uk>
2026-01-22 18:28:21 +0000

    Added Node class to Node.java

--- backend/src/agentnavigation/environment/Node.java
+++ backend/src/agentnavigation/environment/Node.java
@@ -1,5 +1,29 @@
 package agentnavigation.environment;
 
 public class Node {
+    public int row;
+    public int column;
 
+    public Node(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Node)) return false;
+        Node node = (Node) obj;
+        return (this.row == node.row && this.column == node.column);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * this.row + this.column;
+    }
+
+    @Override
+    public String toString() {
+        return "(" +this.row + ", " + this.column + ")";
+    }
 }
