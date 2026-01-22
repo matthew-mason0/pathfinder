2483168b89f8cddffddba8fc907f49c3c35fc721
matthew-mason0 <matthewmason2004@yahoo.co.uk>
2026-01-22 19:23:01 +0000

    Added top-level code to Main.java

--- backend/src/agentnavigation/Main.java
+++ backend/src/agentnavigation/Main.java
@@ -1,16 +1,22 @@
+package agentnavigation;
 /**
  * Entry point for backend
  * Responsible for initialising configuration, selecting environments and algorithms and starting the WebSocket server
  */
 
-package agentnavigation;
-
+import agentnavigation.algorithms.AlgorithmFactory;
 import agentnavigation.algorithms.SearchAlgorithm;
+import agentnavigation.environment.EnvironmentLoader;
+import agentnavigation.environment.Graph;
+import agentnavigation.environment.Node;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println(SearchAlgorithm.class.getName());
+        Graph graph = EnvironmentLoader.createGridGraph(10, 10);
+        SearchAlgorithm algorithm = AlgorithmFactory.createBFS();
+        Node start = new Node(0, 0);
+
+        algorithm.search(graph, start);
     }
 }
