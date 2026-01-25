4f9b543b92dd190f39a2564a27a4a8d45433c68a
matthew-mason0 <matthewmason2004@yahoo.co.uk>
2026-01-25 10:54:24 +0000

    Implemented StepListener in BFS.java

--- backend/src/agentnavigation/algorithms/BFS.java
+++ backend/src/agentnavigation/algorithms/BFS.java
@@ -9,29 +9,48 @@ import java.util.ArrayList;
 
 import agentnavigation.environment.Graph;
 import agentnavigation.environment.Node;
+import agentnavigation.listeners.ConsoleStepListener;
+import agentnavigation.listeners.StepListener;
 
 public class BFS implements SearchAlgorithm {
+
+    private final StepListener listener;
+    public BFS() {
+        this.listener = ConsoleStepListener.INSTANCE;
+    }
+    public BFS(StepListener listener) {
+        this.listener = listener;
+    }
 
     @Override
     public List<Node> search(Graph graph, Node start) {
         Queue<Node> queue = new ArrayDeque<>();
         Set<Node> visited = new HashSet<>();
+        List<Node> traversalList = new ArrayList<>();
+
+        listener.onAlgorithmStart(start);
 
         visited.add(start);
         queue.add(start);
-        List<Node> traversalList = new ArrayList<>();
+        listener.onNodeDiscovered(start);
+        listener.onFrontierUpdate(queue);
 
         while (!queue.isEmpty()) {
             Node current = queue.remove();
+            listener.onNodeExplored(current);
+            listener.onFrontierUpdate(queue);
             traversalList.add(current);
 
             for (Node neighbour : graph.getNeighbours(current)) {
                 if (!visited.contains(neighbour)) {
                     visited.add(neighbour);
                     queue.add(neighbour);
+                    listener.onNodeDiscovered(neighbour);
+                    listener.onFrontierUpdate(queue);
                 }
             }
         }
+        listener.onAlgorithmEnd();
         return traversalList;
     }
 }
