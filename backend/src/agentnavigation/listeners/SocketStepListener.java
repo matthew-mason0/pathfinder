cd52daaf60b089c1b1946ab09cf6e3d863d39d34
matthew-mason0 <matthewmason2004@yahoo.co.uk>
2026-01-29 16:05:02 +0000

    Prepared messages in SocketStepListener.java

---
+++ backend/src/agentnavigation/listeners/SocketStepListener.java
@@ -0,0 +1,95 @@
+package agentnavigation.listeners;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import agentnavigation.environment.Node;
+import agentnavigation.messaging.Message;
+import agentnavigation.messaging.MessageType;
+
+public class SocketStepListener implements StepListener{
+    public static final StepListener INSTANCE = new SocketStepListener();
+    private SocketStepListener() {};
+    
+    @Override
+    public void onAlgorithmStart(Node start) {
+
+        Message message = new Message(
+            MessageType.ALGORITHM_START,
+            nodeIdFormatter(start),
+            null,
+            null
+        );
+    }
+
+    @Override
+    public void onNodeDiscovered(Node node) {
+
+        Message message = new Message(
+            MessageType.NODE_DISCOVERED,
+            nodeIdFormatter(node),
+            null,
+            null
+        );
+    }
+
+    @Override
+    public void onNodeExplored(Node node) {
+
+        Message message = new Message(
+            MessageType.NODE_EXPLORED,
+            nodeIdFormatter(node),
+            null,
+            null
+        );
+    }
+
+    @Override
+    public void onFrontierUpdate(Collection<Node> frontier) {
+        Message message = new Message(
+            MessageType.FRONTIER_UPDATE,
+            null,
+            nodeListFormatter(frontier),
+            null
+        );
+    }
+
+    @Override
+    public void onPathFound(List<Node> path) {
+
+        Message message = new Message(
+            MessageType.PATH_FOUND,
+            null,
+            nodeListFormatter(path),
+            null
+        );
+    }   
+
+    @Override
+    public void onAlgorithmEnd() {
+        Message message = new Message(
+            MessageType.ALGORITHM_END,
+            null,
+            null,
+            null
+        );
+    }
+
+    private List<String> nodeListFormatter(List<Node> nodeList) {
+        List<String> formattedList = new ArrayList<>();
+        for (Node node : nodeList) {
+            formattedList.add(node.toString());
+        }
+        return formattedList;
+    }
+    private List<String> nodeListFormatter(Collection<Node> nodeList) {
+        List<Node> list = new ArrayList<>(nodeList);
+        return nodeListFormatter(list);
+    }
+
+    private String nodeIdFormatter(Node node) {
+        Integer nodeId = node.getId();
+        return nodeId.toString();
+    }
+}
