d1fbaf556f0339efee11d7f278d1b705fced0f97
matthew-mason0 <matthewmason2004@yahoo.co.uk>
2026-01-30 11:23:26 +0000

    Exposed node row/column in Message.java

--- backend/src/agentnavigation/messaging/Message.java
+++ backend/src/agentnavigation/messaging/Message.java
@@ -5,18 +5,24 @@ import java.util.List;
 public class Message {
     private final MessageType type;
     private final String nodeId;
+    private final String nodeRow;
+    private final String nodeColumn;
     private final List<String> nodeList;
     private final String info;
 
-    public Message(MessageType type, String nodeId, List<String> nodeList, String info) {
+    public Message(MessageType type, String nodeId, String nodeRow, String nodeColumn, List<String> nodeList, String info) {
         this.type = type;
         this.nodeId = nodeId;
+        this.nodeRow = nodeRow;
+        this.nodeColumn = nodeColumn;
         this.nodeList = nodeList;
         this.info = info;        
     }
 
-    public MessageType getType() { return type; }
-    public String getNodeId() { return nodeId; }
-    public List<String> getNodeList() { return nodeList; }
-    public String getInfo() { return info; }
+    public MessageType getType() { return this.type; }
+    public String getNodeId() { return this.nodeId; }
+    public String getNodeRow() { return this.nodeRow; }
+    public String getNodeColumn() { return this.nodeColumn; }
+    public List<String> getNodeList() { return this.nodeList; }
+    public String getInfo() { return this.info; }
 }
