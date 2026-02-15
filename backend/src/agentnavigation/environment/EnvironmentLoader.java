357cec6e3a690f3a5ab636bd0565638aedc99bef
matthew-mason0 <matthewmason2004@yahoo.co.uk>
2026-02-15 21:06:48 +0000

    Changed inital end node

--- backend/src/agentnavigation/environment/EnvironmentLoader.java
+++ backend/src/agentnavigation/environment/EnvironmentLoader.java
@@ -4,7 +4,7 @@ public class EnvironmentLoader {
     public static Environment createGridEnvironment(int rows, int columns) {
         GridGraph graph = new GridGraph(rows, columns);
         Node start = graph.getNodeAt(0, 0); // TODO allow for start node choice
-        Node end = graph.getNodeAt(6, 7); // TODO allow for end node choice
+        Node end = graph.getNodeAt(9, 9); // TODO allow for end node choice
         return new Environment(graph, start, end);
     }
 }
