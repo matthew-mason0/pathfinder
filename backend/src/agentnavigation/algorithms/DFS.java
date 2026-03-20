065ccb8f7f861bfa31c471dc24c4ec95cbf783a9
matthew-mason0 <matthewmason2004@yahoo.co.uk>
2026-03-20 20:06:30 +0000

    Removed unused imports

--- backend/src/agentnavigation/algorithms/DFS.java
+++ backend/src/agentnavigation/algorithms/DFS.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.Stack;
 import java.util.HashSet;
 
 import agentnavigation.environment.Graph;
